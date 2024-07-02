593123f37505156981d3063488251443b89aab6e
Masui <22im0147@i-u.ac.jp>
2024-07-02 14:21:20 +0900

    ホーム画面のシフト表の表示形式を整えた。外部スタイルシートを使う方法でCSSを書いた。

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -2,6 +2,7 @@ class ShiftsController < ApplicationController
     def index
         @decision_attendances = DecisionAttendance.includes(:user).all
         @decision_attendances_by_date = @decision_attendances.group_by { |decision_attendances| decision_attendances.employee_work_time_start.to_date } 
+        @dates = (Date.today.beginning_of_month..Date.today.end_of_month).to_a
         @users = User.select(:id, :employee_name, :employee_name_kana)
         @show_special_link = (current_user.id == 3)
     end
